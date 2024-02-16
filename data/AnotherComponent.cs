a9899fe10fa8dbfd946f5e742d6ad681c2b28f67
LazyUnigineDude <lazy.unigine@gmail.com>
2024-02-15 22:01:13 -0500

    Add project Files

---
+++ data/AnotherComponent.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unigine;
+
+[Component(PropertyGuid = "889570f416a111b1ed4e1f697dbaf2b2a4964dce")]
+public class AnotherComponent : Component
+{
+    void Init()
+	{
+		// write here code to be called on component initialization
+		
+	}
+	
+	void Update()
+	{
+		// write here code to be called before updating each render frame
+	}
+}
