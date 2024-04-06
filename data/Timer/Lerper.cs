f3183a4ed5de14c85e596c02da875757d204a261
LazyUnigineDude <lazy.unigine@gmail.com>
2024-04-06 19:40:49 -0400

    Add

--- data/Timer/Lerper.cs
+++ data/Timer/Lerper.cs
@@ -1,11 +1,11 @@
-﻿using Unigine;
+﻿using System.Runtime.CompilerServices;
+using Unigine;
 
 namespace UnigineApp.data.Timer
 {
     internal static class Lerper
     {
         public enum LERP_TYPE { LINEAR, EASE_IN, EASE_OUT, EASE_IN_OUT };
-
         public static dvec3 Lerp(dvec3 Start, dvec3 End, float t, LERP_TYPE Type = LERP_TYPE.LINEAR) 
         { 
             switch (Type)
@@ -15,6 +15,15 @@ namespace UnigineApp.data.Timer
                 case LERP_TYPE.EASE_IN_OUT: return MLerp(Start, End, EInOut(t));
                 default:                    return MLerp(Start, End, t);
             }
+        }
+
+        public static dvec3 PingPong(dvec3 Start, dvec3 End, float t, float Repeat)
+        {
+            return new dvec3(
+                PingPong(Start.x, End.x, t, Repeat),
+                PingPong(Start.y, End.y, t, Repeat),
+                PingPong(Start.z, End.z, t, Repeat)
+                );
         }
 
         private static dvec3 MLerp(dvec3 Start, dvec3 End, float t)
@@ -31,6 +40,15 @@ namespace UnigineApp.data.Timer
         private static float EIn(float t) { return Square(t); }
         private static float EOut(float t) { return Flip(Square(Flip(t))); }
         private static float EInOut(float t) { return Lerp(EIn(t), EOut(t), t) ; }
+        private static double PingPong(double min, double max, float t, float repeat) {
+
+            double hRepeat = repeat * 0.5;
+            double phase = t % repeat;
+            double slope = (max - min) / hRepeat;
+
+            if(phase < hRepeat) { return min + (slope * phase); }
+            return max - (slope * (phase - hRepeat));
+        }
 
         private static float Flip(float t) { return 1 - t; }
         private static float Square(float t) { return t * t; }
--- data/Timer/TimeController.cs
+++ data/Timer/TimeController.cs
@@ -19,6 +19,9 @@ public class TimeController : Component
 	private float Time = 0;
 	private List<dvec3> Start = new();
 	private dvec3 End = dvec3.LEFT * -4;
+	bool canAnimate = false;
+
+	private WidgetButton StartAnim;
 
     private void Init()
 	{
@@ -43,6 +46,11 @@ public class TimeController : Component
 		TimeSlider.EventChanged.Connect(() => { Time = 0; });
 		GUI.AddChild(TimeSlider, Gui.ALIGN_EXPAND | Gui.ALIGN_OVERLAP);
 
+		StartAnim = new WidgetButton("Play");
+		StartAnim.SetPosition(100, 250);
+		StartAnim.EventClicked.Connect(() => { canAnimate = true; });
+		GUI.AddChild(StartAnim, Gui.ALIGN_EXPAND | Gui.ALIGN_OVERLAP);
+
 		foreach (var item in Objects) Start.Add(item.WorldPosition);
     }
 
@@ -51,7 +59,7 @@ public class TimeController : Component
     private void Update()
 	{
 		UpdateL.Text = "Update: " + Game.IFps + " Frames";
-		MoveObjects();
+		if(canAnimate) MoveObjects();
     }
 
     private void MoveObjects()
@@ -63,10 +71,11 @@ public class TimeController : Component
 		Objects[1].WorldPosition = Lerper.Lerp(Start[1], Start[1] + End, t, Lerper.LERP_TYPE.EASE_IN);
 		Objects[2].WorldPosition = Lerper.Lerp(Start[2], Start[2] + End, t, Lerper.LERP_TYPE.EASE_OUT);
 		Objects[3].WorldPosition = Lerper.Lerp(Start[3], Start[3] + End, t, Lerper.LERP_TYPE.EASE_IN_OUT);
+		Objects[4].WorldPosition = Lerper.PingPong(Start[4], Start[4] + End, t, 1);
 
 		float tCurve = Curve.Evaluate(t);
-       Objects[4].WorldPosition = Lerper.Lerp(Start[4], Start[4] + End, tCurve, Lerper.LERP_TYPE.LINEAR);
-		if (t > 1.0f) Time = 0.0f;
+        Objects[5].WorldPosition = Lerper.Lerp(Start[5], Start[5] + End, tCurve, Lerper.LERP_TYPE.LINEAR);
+		if (t > 1.0f) { Time = 0.0f; canAnimate = false; }
     }
 
 	private void Shutdown()
@@ -77,6 +86,7 @@ public class TimeController : Component
 		if (GUI.IsChild(UpdateL) == 1) { GUI.RemoveChild(UpdateL); UpdateL.DeleteLater(); } 
 		if (GUI.IsChild(PhysicsL) == 1) { GUI.RemoveChild(PhysicsL); PhysicsL.DeleteLater(); } 
 		if (GUI.IsChild(TimeSlider) == 1) { GUI.RemoveChild(TimeSlider); TimeSlider.DeleteLater(); }
+		if (GUI.IsChild(StartAnim) == 1) { GUI.RemoveChild(StartAnim); StartAnim.DeleteLater(); }
     }
 
 }
