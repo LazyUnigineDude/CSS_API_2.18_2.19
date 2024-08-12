edf004276bfdf8126ff876b725feb4bb0656c41a
LazyUnigineDude <lazy.unigine@gmail.com>
2024-08-12 18:26:43 -0400

    Add

---
+++ data/GUI/ChessGUI.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unigine;
+
+[Component(PropertyGuid = "f18d254471ec733d447d5ca601151c05710aa908")]
+public class ChessGUI : Component
+{
+	void Init()
+	{
+		// write here code to be called on component initialization
+		
+	}
+	
+	void Update()
+	{
+		// write here code to be called before updating each render frame
+		
+	}
+}
---
+++ data/GUI/Widgets.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unigine;
+
+[Component(PropertyGuid = "a044ac6f354bab8943cef6e51922ded8908f4fc9")]
+public class Widgets : Component
+{
+	void Init()
+	{
+		// write here code to be called on component initialization
+		
+	}
+	
+	void Update()
+	{
+		// write here code to be called before updating each render frame
+		
+	}
+}
