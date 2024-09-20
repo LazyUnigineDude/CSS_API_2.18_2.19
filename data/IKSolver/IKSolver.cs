dd237dad53d6a1a470bb7120bb7ad5826415ca35
LazyUnigineDude <lazy.unigine@gmail.com>
2024-09-20 18:57:26 -0400

    Inverse Kinematics Solver

---
+++ data/IKSolver/IKSolver.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unigine;
+
+[Component(PropertyGuid = "31d6c4d5d2d76cab0f580ee2948e7f696d153543")]
+public class IKSolver : Component
+{
+	void Init()
+	{
+		// write here code to be called on component initialization
+		
+	}
+	
+	void Update()
+	{
+		// write here code to be called before updating each render frame
+		
+	}
+}
