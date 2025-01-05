a380ecab39f43d4446d7e742aa614e751478843a
LazyUnigineDude <lazy.unigine@gmail.com>
2025-01-05 11:38:52 -0500

    Add

--- data/Rope/RopeHandler.cs
+++ data/Rope/RopeHandler.cs
@@ -5,6 +5,8 @@ using Unigine;
 [Component(PropertyGuid = "85ea0e7d57829f4fd559d1faa76715c34104cee8")]
 public class RopeHandler : Component
 {
+
+
 	void Init()
 	{
 		// write here code to be called on component initialization
